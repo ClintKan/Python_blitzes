96c283fe3920dafcb1c92c26ae3c2ee3cd3d96ba
Home <home@Homes-MBP.lan>
2024-09-28 05:07:39 -0400

    reps

---
+++ reverse_printing.py
@@ -0,0 +1,10 @@
+# A script that prints the reverse of what's been input
+
+#Intro to user
+print("Welcome to the script that prints the reverse of what's been input\n")
+
+#Ask user for input
+usr_input = input("Input the word(s) you'd like to see reverse printed\n")
+
+#Print in reverse
+print(usr_input[::-1])
